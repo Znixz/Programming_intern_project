1d6090458fda3667b0e8151e4d873fe693bfd07a
Znixz <zimplict@gmail.com>
2025-08-16 19:12:32 +0100

    List_opration.py

---
+++ List_opration.py
@@ -0,0 +1,12 @@
+my_list = [1,2,3,4,5,6]
+list_count = len(my_list)
+print (list_count)
+my_list.sort()
+print(my_list)
+my_list.sort(reverse = True)
+print(my_list)
+last_item = my_list[-1]
+print(last_item)
+mid_index = len(my_list) // 2 - 1
+mid_item = my_list[mid_index]
+print(float(mid_item))
